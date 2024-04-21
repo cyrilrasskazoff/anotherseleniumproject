615b67132cc512a589faee003c6f937336a26a48
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-04-21 13:24:57 +0300

    minor refactoring

--- lesson_5/find_lesson.py
+++ lesson_5/find_lesson.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 service = Service(executable_path=ChromeDriverManager().install())
@@ -8,6 +9,7 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://freeconferencecall.com/login")
 driver.set_window_size(1440, 900)
+print(type(driver.find_element(By.ID, "loginformsubmit"))) # <class 'selenium.webdriver.remote.webelement.WebElement'>
 print(type(driver.find_element("id", "loginformsubmit")))  # <class 'selenium.webdriver.remote.webelement.WebElement'>
 driver.find_element("id", "loginformsubmit").click()
 
