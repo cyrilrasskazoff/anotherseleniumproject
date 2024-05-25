4fdcfb10e1bb88b158bbe91904e93f59bb185e9d
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-05-25 17:46:23 +0300

    xpath база

---
+++ lesson_7/xpath.py
@@ -0,0 +1,7 @@
+# ➖ Глобальный поиск - //header
+# ➖ Вложенность - //header/div/button , тот же результат вернет: //header//button
+# ➖ Порядковый номер - (//header/div)[2]
+# ➖ Поиск по атрибуту - //input[@type='submit']
+# ➖ Поиск по параметру text - //button[text()='Sign in']
+# ➖ Поиск по содержимому - //button[contains(@class, 'btn')]
+# ➖ Поиск по нескольким атрибутам - //input[@type='email' and @role='login']
