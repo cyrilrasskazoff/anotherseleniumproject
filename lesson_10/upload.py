3320e95a92ad27a1811b41b89799e402f8c3e5d9
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-05-30 12:42:27 +0300

    upload файла

---
+++ lesson_10/upload.py
@@ -0,0 +1,14 @@
+import os
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+service = Service(executable_path=ChromeDriverManager().install())
+
+driver = webdriver.Chrome(service=service)
+# загрузка файла осуществляется через unout у которого type = "file", поэтому нужео искать этот элемент на странице
+driver.get("https://the-internet.herokuapp.com/upload")
+time.sleep(3)
+choose_file_btn = driver.find_element("xpath", "//input[@id='file-upload']")
+choose_file_btn.send_keys(f"{os.getcwd()}/downloads/Student.json")
+time.sleep(3)
