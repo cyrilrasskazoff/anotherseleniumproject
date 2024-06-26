8c0f846ebaea50b2f0a7db78b52157d675600e40
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-06-26 17:58:25 +0300

    цепочки действий drag and drop

--- lesson_21/actin_chains_2.py
+++ lesson_21/actin_chains_2.py
@@ -3,19 +3,13 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.add_argument("--window-size=1920,1080")
-
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(options=options, service=service)
-
 actions = ActionChains(driver)
-wait = WebDriverWait(driver, 15, poll_frequency=1)
 
 # простой drag-and-drop с помощью drag_and_drop(source, target)
 driver.get("https://the-internet.herokuapp.com/drag_and_drop")
