4c851ea916049fda2a29d74dc5468fcf97be2332
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2026-06-30 16:56:16 +0300

    minor refactoring in alerts module

--- lesson_13/alerts.py
+++ lesson_13/alerts.py
@@ -3,7 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 import time
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
 service = Service(executable_path=ChromeDriverManager().install())
 chrome_options = webdriver.ChromeOptions()
@@ -18,16 +18,18 @@ driver.get("https://demoqa.com/alerts")
 
 # alert с одной кнопкой (OK)
 BUTTON_1 = ("xpath", "//button[@id='alertButton']")
-wait.until(EC.element_to_be_clickable(BUTTON_1)).click()
-alert = wait.until(EC.alert_is_present())
+wait.until(ec.element_to_be_clickable(BUTTON_1)).click()
+alert = wait.until(ec.alert_is_present())
 driver.switch_to.alert
+time.sleep(5)
 alert.accept()
 
 # alert с двумя кнопками - Confirm - (OK/Cancel)
 BUTTON_3 = ("xpath", "//button[@id='confirmButton']")
-wait.until(EC.element_to_be_clickable(BUTTON_3)).click()
-alert = wait.until(EC.alert_is_present())
+wait.until(ec.element_to_be_clickable(BUTTON_3)).click()
+alert = wait.until(ec.alert_is_present())
 driver.switch_to.alert
+time.sleep(5)
 # получить текст из alert-a:
 print(alert.text)
 alert.dismiss()
@@ -35,11 +37,11 @@ alert.dismiss()
 
 # alert с полем ввода - Prompt - (OK/Cancel)
 BUTTON_4 = ("xpath", "//button[@id='promtButton']")
-wait.until(EC.element_to_be_clickable(BUTTON_4)).click()
-alert = wait.until(EC.alert_is_present())
+wait.until(ec.element_to_be_clickable(BUTTON_4)).click()
+alert = wait.until(ec.alert_is_present())
 driver.switch_to.alert
 # ввести текст в alert:
 alert.send_keys("Hello")
-time.sleep(3)
+time.sleep(5)
 alert.accept()
 
