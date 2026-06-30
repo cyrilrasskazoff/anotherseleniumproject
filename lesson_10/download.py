476a9db151b32a3e0c692db5e214661bc5f5ee17
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2026-06-30 17:34:05 +0300

    added some explainatoty comment into download.py

--- lesson_10/download.py
+++ lesson_10/download.py
@@ -10,6 +10,17 @@ chrome_options = webdriver.ChromeOptions()
 download_prefs = {
     "download.default_directory": f"{os.getcwd()}/downloads"
 }
+"""
+Значение ключа задается через f-строку, чтобы автоматически объединить статическую часть пути (/downloads) с 
+динамической текущей директорией проекта.
+🔍 Почему это нужно?
+os.getcwd() возвращает путь. Эта функция вычисляет место, где находится запущенный Python-скрипт (например, 
+/Users/admin/project/main.py). Это может быть любой абсолютный путь у каждого пользователя или на каждом компьютере.
+Нужно добавить папку downloads. Вы хотите скачивать файлы не в системную папку загрузок браузера (~/Downloads), а именно
+в подпапку проекта, чтобы не засорять диск и легко находить результаты.
+Сборка пути. Вам нужно превратить путь из os.getcwd() (например, /var/www/site) в итоговый строку 
+/var/www/site/downloads.
+"""
 chrome_options.add_experimental_option("prefs", download_prefs)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
