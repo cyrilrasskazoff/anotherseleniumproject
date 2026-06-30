b29654d67cf7cd7e487646809f8c5376b17a40ec
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2026-06-30 17:10:38 +0300

    minor refactoring in alerts module

--- lesson_13/alerts.py
+++ lesson_13/alerts.py
@@ -19,16 +19,14 @@ driver.get("https://demoqa.com/alerts")
 # alert с одной кнопкой (OK)
 BUTTON_1 = ("xpath", "//button[@id='alertButton']")
 wait.until(ec.element_to_be_clickable(BUTTON_1)).click()
-alert = wait.until(ec.alert_is_present())
-driver.switch_to.alert
+alert = wait.until(ec.alert_is_present()).driver.switch_to.alert
 time.sleep(5)
 alert.accept()
 
 # alert с двумя кнопками - Confirm - (OK/Cancel)
 BUTTON_3 = ("xpath", "//button[@id='confirmButton']")
 wait.until(ec.element_to_be_clickable(BUTTON_3)).click()
-alert = wait.until(ec.alert_is_present())
-driver.switch_to.alert
+alert = wait.until(ec.alert_is_present()).driver.switch_to.alert
 time.sleep(5)
 # получить текст из alert-a:
 print(alert.text)
@@ -38,8 +36,7 @@ alert.dismiss()
 # alert с полем ввода - Prompt - (OK/Cancel)
 BUTTON_4 = ("xpath", "//button[@id='promtButton']")
 wait.until(ec.element_to_be_clickable(BUTTON_4)).click()
-alert = wait.until(ec.alert_is_present())
-driver.switch_to.alert
+alert = wait.until(ec.alert_is_present()).driver.switch_to.alert
 # ввести текст в alert:
 alert.send_keys("Hello")
 time.sleep(5)
